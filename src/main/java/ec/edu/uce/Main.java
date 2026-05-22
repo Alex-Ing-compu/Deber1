ae73be1550f30189f2317c0de1abbd86cf251854
Alex-Ing-compu <jhonalex2002cordova@gmail.com>
2026-05-22 14:36:04 -0500

    Deber_Unidad 2_CRUD jakarta

--- src/main/java/ec/edu/uce/Main.java
+++ src/main/java/ec/edu/uce/Main.java
@@ -3,7 +3,9 @@ package ec.edu.uce;
 import java.time.LocalDate;
 
 import ec.edu.uce.aplication.service.EstudianteService;
+import ec.edu.uce.aplication.service.ProfesorService;
 import ec.edu.uce.damain.model.Estudiante;
+import ec.edu.uce.damain.model.Profesor;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
@@ -14,47 +16,60 @@ public class Main {
     public static void main(String[] args) {
         Quarkus.run(App.class, args);
     }
- 
-    public static class App implements QuarkusApplication   {
+
+    public static class App implements QuarkusApplication {
 
         @Inject
-            private EstudianteService estudianteService;
+        private EstudianteService estudianteService;
 
-
-
+        @Inject
+        private ProfesorService profesorService;
 
         @Override
         public int run(String... args) throws Exception {
 
-    
             System.out.println("Conexion a la base de datos POSTGRESQL!!");
 
+            // CREAR ESTUDIANTE
             Estudiante estudiante = new Estudiante();
-            estudiante.setNombre("Pedro");
-            estudiante.setApellido("Perez");
-            estudiante.setFechaNacimiento(LocalDate.of(2005, 01, 10));
+            estudiante.setNombre("Jhon");
+            estudiante.setApellido("Cordova");
+            estudiante.setFechaNacimiento(LocalDate.of(2003, 06, 10));
             estudiante.setGenero("M");
-            
-            //Guardar
+
+            // CREAR PROFESOR
+            Profesor profe = new Profesor();
+            profe.setNombre("Andy");
+            profe.setApellido("Suquilandy");
+            profe.setEspecialidad("Civil");
+            profe.setCorreo("andy@uce.edu.ec");
+
+            // Guardar
             this.estudianteService.guardar(estudiante);
 
-            //Actualizar
-           // Estudiante estudiante2 = this.estudianteService.buscarPorId(4);
-           // estudiante2.setNombre("PAULLLLLLLLLL");
-           // estudiante2.setApellido("LLUMI");
+            // Guardar
+            this.profesorService.guardar(profe);
 
-           // this.estudianteService.actualizar(estudiante2);
+            //Bucasr profe por ID
+            System.out.println(this.profesorService.buscarPorId(2).toString());
+            
 
-            //Eliminar
-            //this.estudianteService.eliminar(2);
+            // Actualizar
+            // Estudiante estudiante2 = this.estudianteService.buscarPorId(4);
+            // estudiante2.setNombre("PAULLLLLLLLLL");
+            // estudiante2.setApellido("LLUMI");
 
-            //Bucascar porm ID
-            System.out.println(this.estudianteService.buscarPorId(1).toString());
-            //this.estudianteService.buscarPorId(1);
+            // this.estudianteService.actualizar(estudiante2);
+
+            // Eliminar
+            // this.estudianteService.eliminar(2);
+
+            // Bucascar porm ID
+            // System.out.println(this.estudianteService.buscarPorId(1).toString());
+            // this.estudianteService.buscarPorId(1);
 
             return 0;
         }
-   
-       
+
     }
 }
--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO estudiante (
-    estu_id, estu_apellido,
-    estu_fecha_nacimiento,
-    estu_nombre,
-    estu_genero)
-    
-VALUES (1, 'Cordova', '2002-03-27', 'Jhon', 'M');
